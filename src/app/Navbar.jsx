2e3df39d0a7038402783cb5b65487652ed1d9c9b
DeadStalker69 <106471305+DeadStalker69@users.noreply.github.com>
2024-01-05 23:04:28 +0530

    duplicate files

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+//import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectToDB = async()=> {
-    await mongoose.connect(process.env.MongoDB_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(()=> {
         console.log("App connected to database")
     })
